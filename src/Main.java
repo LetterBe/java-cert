951e7d5d15032b1a0e83b5ae0d7754f5eab0a5e9
Barbara <barbarap@tutanota.com>
2022-12-09 22:33:08 +0100

    toString

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public String toString() {
+        // com o metodo toString() podemos retornar uma string quando chamamos o objeto
+        return "Hello, World!";
     }
+    public static void main(String[] args) {
+        // java convert pra String com metodo toString()
+        System.out.println(new Main());
+    }
+
 }
